d9b2247e3c367853f991a8240c07e68502f430a6
H Jivan Singha <hjivansingha123@gmail.com>
2026-03-17 16:16:19 +0530

    blacklist.js improvement

--- layers/layer_1/02_blacklist.js
+++ layers/layer_1/02_blacklist.js
@@ -11,15 +11,31 @@ Sources:
 
 This list is stored in chrome.storage so it persists.
 */
-export async function isBlacklisted(domain) {
+import { getOpenPhishFeed } from "../../services/blacklistFeeds.js";
 
+export async function isBlacklisted(domain, url) {
+
+    /* 1. Local blacklist */
     const data = await chrome.storage.local.get(["blacklist"]);
-
     const blacklist = data.blacklist || [];
 
     if (blacklist.includes(domain)) {
         return true;
     }
 
+    /* 2. External feed */
+    const feed = await getOpenPhishFeed();
+
+    // safer match
+    const isMatch = feed.some(phishUrl => {
+        return url === phishUrl || url.startsWith(phishUrl);
+    });
+
+    if (isMatch) {
+        return true;
+    }
+
     return false;
 }
+
+
